60557f794e209d1599cedcba2b0893416af4bee3
hayondan <79042979+hayondan@users.noreply.github.com>
2021-02-25 15:00:34 +0200

    Update clean_enviroment.py

--- clean_enviroment.py
+++ clean_enviroment.py
@@ -1,4 +1,7 @@
 import requests
 
-requests.get('http://127.0.0.1:5000/stop_server')
-requests.get('http://127.0.0.1:5001/stop_server')
+try:
+  requests.get('http://127.0.0.1:5000/stop_server')
+  requests.get('http://127.0.0.1:5001/stop_server')
+    except:
+        return False
